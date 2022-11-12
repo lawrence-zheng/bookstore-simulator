f5119ca3a2a9c83bb4f030ef8daebf9820878fa4
Lawrence Zheng <lxz2@andrew.cmu.edu>
2022-11-12 15:34:44 -0500

    Added book data
    Added basic book data in booksData.js

---
+++ js/booksData.js
@@ -0,0 +1,82 @@
+const books = {
+    "Sea of Monsters": {
+        "description": "an adventure novel about a young hero battling mythical creatures",
+        "size": "medium",
+        "color": {
+            "rgba": [52, 141, 209, 1],
+            "colorText": "blue",
+        },
+        "defaultValue": 0,
+        "money": 10
+    },
+    "Hearty Dinners": {
+        "description": "a cookbook containing recipes from many different cultures, written by celebrity chef Gordino Ramsee",
+        "size": "large",
+        "color": {
+            "rgba": [143, 39, 12, 1],
+            "colorText": "reddish brown",
+        },
+        "defaultValue": 3,
+        "money": 15
+    },
+    "Rosaline": {
+        "description": "a semi autobiographical novel about famous actress and model Rosaline, detailing aspects of her love life and depression",
+        "size": "medium",
+        "color": {
+            "rgba": [143, 12, 126, 1],
+            "colorText": "purple",
+        },
+        "defaultValue": 4,
+        "money": 15
+    },
+    "Vampire Moon": {
+        "description": "a young adult novel, presenting the steamy romance between a young girl and a dark, brooding vampire",
+        "size": "medium",
+        "color": {
+            "rgba": [122, 94, 119, 1],
+            "colorText": "purplish grey",
+        },
+        "defaultValue": -4,
+        "money": 8
+    },
+    "Agent 070": {
+        "description": "a spy infiltrates a criminal syndicate, using his quick wit and handy grappling hook to get out of sticky situations",
+        "size": "medium",
+        "color": {
+            "rgba": [64, 128, 192, 1],
+            "colorText": "blue",
+        },
+        "defaultValue": 1,
+        "money": 10
+    },
+    "Girl Meets Girl": {
+        "description": "an LGBTQ+ romance novel where a small-town girl falls hopeleslly in love with the girl who sits next to her in art class",
+        "size": "medium",
+        "color": {
+            "rgba": [226, 157, 190, 1],
+            "colorText": "reddish pink",
+        },
+        "defaultValue": 2,
+        "money": 10
+    },
+    "Top 10 Tips to Score a Hot Date": {
+        "description": "a magazine giving makeup tips and fashion advice",
+        "size": "magazine",
+        "color": {
+            "rgba": [255, 40, 215, 1],
+            "colorText": "bright pink",
+        },
+        "defaultValue": 2,
+        "money": 5
+    },    
+    "Data Visualization for Dumdums": {
+        "description": "a textbook giving easy to understand explanations of how to get started with data visuallization",
+        "size": "large",
+        "color": {
+            "rgba": [249, 241, 67, 1],
+            "colorText": "yellow",
+        },
+        "defaultValue": -4,
+        "money": 20
+    },    
+};
