10f56b7b9a6af608186defa0f6e2266dbbd955cc
Lawrence Zheng <lxz2@andrew.cmu.edu>
2022-11-22 10:07:57 -0500

    Smoothed out movement
    Can no longer move to a new location when movement command is in progress

--- js/script.js
+++ js/script.js
@@ -18,58 +18,86 @@ document.querySelector(".bookstore-name").innerHTML = storeName;
 //movement commands
 
 let playerPos = 0;
+let movementDisabled = false;
 player = document.querySelector("#protag");
 player.style.transitionDuration = "0s";
 
 function moveToCheckout() {
     console.log("move to checkout")
-    if (playerPos == 0) {
-        player.style.transitionDuration = "0s";
-        checkout1();
-    } else if (playerPos == 1){
-        player.style.transitionDuration = "3s";
-    } else if (playerPos == 2){
-        player.style.transitionDuration = "4s";
+    if (!movementDisabled) {
+        movementDisabled = true;
+        let delay = 0;
+        if (playerPos == 0) {
+            player.style.transitionDuration = "0s";
+            checkout1();
+        } else if (playerPos == 1){
+            player.style.transitionDuration = "3s";
+            delay = 3000;
+        } else if (playerPos == 2){
+            player.style.transitionDuration = "4s";
+            delay = 2000;
+        }
+        player.style.paddingLeft = "220px";
+        hideDialogue();
+        hideBookshelf();
+        setTimeout(() => playerPos = 0, delay);
+        setTimeout(() => movementDisabled = false, delay);
+
+    } else {
+        console.log("could not move because there is already a movement command");
     }
-    player.style.paddingLeft = "220px";
-    setTimeout(() => playerPos = 0, 1000);
-    hideDialogue();
-    hideBookshelf();
+
     
 }
 
 function moveToRecCorner() {
     console.log("move to rec corner")
-
-    if (playerPos == 0) {
-        player.style.transitionDuration = "3s";
-    } else if (playerPos == 1){
-        player.style.transitionDuration = "0s";
-    } else if (playerPos == 2){
-        player.style.transitionDuration = "1.5s";
+    if (!movementDisabled) {
+        movementDisabled = true;
+        let delay = 0;
+        if (playerPos == 0) {
+            player.style.transitionDuration = "3s";
+            delay = 3000;
+        } else if (playerPos == 1){
+            player.style.transitionDuration = "0s";
+        } else if (playerPos == 2){
+            player.style.transitionDuration = "1.5s";
+            delay = 1500;
+        }
+        player.style.paddingLeft = "780px";
+        setTimeout(() => playerPos = 1, delay);
+        setTimeout(() => movementDisabled = false, delay);
+        setTimeout(() => showDialogue(), delay);
+        hideBookshelf();
+    } else {
+        console.log("could not move because there is already a movement command");
     }
-    player.style.paddingLeft = "780px";
-    playerPos = 1;
-    showDialogue();
-    hideBookshelf();
     
 }
 
 
 function moveToBookshelf() {
     console.log("move to bookshelf")
-
-    if (playerPos == 0) {
-        player.style.transitionDuration = "4s";
-    } else if (playerPos == 1){
-        player.style.transitionDuration = "1.5s";
-    } else if (playerPos == 2){
-        player.style.transitionDuration = "0s";
+    if (!movementDisabled) {
+        let delay = 0;
+        if (playerPos == 0) {
+            player.style.transitionDuration = "4s";
+            delay = 4000;
+        } else if (playerPos == 1){
+            player.style.transitionDuration = "1.5s";
+            delay = 1500;
+        } else if (playerPos == 2){
+            player.style.transitionDuration = "0s";
+        }
+        player.style.paddingLeft = "980px";
+        playerPos = 2;
+        setTimeout(() => playerPos = 2, delay);
+        setTimeout(() => movementDisabled = false, delay);
+        setTimeout(() => showBookshelf(), delay);
+        hideDialogue();
+    } else {
+        console.log("could not move because there is already a movement command");
     }
-    player.style.paddingLeft = "980px";
-    playerPos = 2;
-    hideDialogue();
-    setTimeout(() =>  showBookshelf(), 1000);
     
 }
 
